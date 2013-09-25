0c2fa77eb5c601d7607a60ba9ba9eab9e9dc030a
Håkon Ødegård Løvdal <hakloev@gmail.com>
2013-09-25 15:44:45 +0200

    Removed libs

--- src/gui/MapPanel.java
+++ src/gui/MapPanel.java
@@ -13,7 +13,7 @@ import javafx.scene.web.WebView;
 import javafx.scene.web.WebViewBuilder;
 
 /**
- * Klasse for å generere kartpanel
+ * Klasse for �� generere kartpanel
  * 
  * @author Max er best
  */
@@ -56,13 +56,13 @@ public class MapPanel extends JPanel {
 	    return webView;
 	}
 	
-//	public static void main(String[] args) {
-//		SwingUtilities.invokeLater(new Runnable() {
-//		
-//		@Override
-//		public void run() {
-//			MapPanel p = new MapPanel();
-//		}
-//	});
-//   }
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+		
+		@Override
+		public void run() {
+			MapPanel p = new MapPanel();
+		}
+	});
+   }
 }
