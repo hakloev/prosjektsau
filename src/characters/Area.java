b5e6bb259a0bf2e62f1693576822572dd7ef926f
maxmelander <maxmelander@dhcp-058055.wlan.ntnu.no>
2013-10-02 15:46:25 +0200

    Area class yo

--- src/characters/Area.java
+++ src/characters/Area.java
@@ -1,11 +1,51 @@
 package characters;
 
+import java.awt.Polygon;
 import java.util.ArrayList;
 
 public class Area {
 	private String name;
-	private ArrayList<Position> PolyPoints;
-	private int SheepCount;
+	private ArrayList<Position> polyPoints;
+	private int sheepCount;
+	private Polygon areaPoly;
+	private static final int DOUBLEDESTROYER = 10000;
 	
+	/**
+	 * 
+	 * @param name The area name
+	 * @param sheepCount Number of sheep in area
+	 */
+	public Area(String name, int sheepCount){
+		setName(name);
+		polyPoints = new ArrayList<Position>();
+		areaPoly = new Polygon();
+	}
+	
+	private void setName(String name){
+		this.name = name;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public void setSheepCount(int count){
+		this.sheepCount = count;
+	}
+	
+	public int getSheepCount(){
+		return sheepCount;
+	}
+	
+	public void addPolyPoint(Position position){
+		polyPoints.add(position);
+	}
+	
+	public void createPoly(){
+		for (Position pos : polyPoints){
+			areaPoly.addPoint((int)(pos.getLongtidude() * DOUBLEDESTROYER), (int)(pos.getLatitude() * DOUBLEDESTROYER));
+		}
+		
+	}
 	
 }
