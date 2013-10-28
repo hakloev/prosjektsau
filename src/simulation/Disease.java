09ac795c73f6ef284c793adebd1657e94e37274d
maxmelander <maxmelander@192.168.1.7>
2013-10-28 12:57:35 +0100

    hmmmm

--- src/simulation/Disease.java
+++ src/simulation/Disease.java
@@ -1,29 +1,39 @@
 package simulation;
 
+/**
+ * 
+ * @author maxmelander
+ *
+ */
 public class Disease {
 	private double spreadDistance;
 	private int spreadChance;
 	private int damage;
-	private int incubationPeriode;
+	private int incubationPeriod;
 	private int diseaseLength;
 	
-	public Disease(double distance, int chance, int damage, int periode, int length){
+	/**
+	 * 
+	 * @param distance The max distance between sheep for the disease to spread
+	 * @param chance The chance of the disease spreading when two sheep inside distance
+	 * @param damage The damage the disease does each simulation update
+	 * @param period The incubation period, where spread happens but no damage is done
+	 * @param length How long the disease lasts
+	 */
+	public Disease(double distance, int chance, int damage, int period, int length){
 		this.spreadDistance = distance / 10;
 		this.spreadChance = chance;
 		if (damage == 0){
 			this.damage = 5;
 		}
-		else if(damage == 1){
-			this.damage = 10;
+		
+		this.damage = damage;
+		
+		if (period >= length - 5){
+			period = 0;
 		}
 		else{
-			this.damage = 15;
-		}
-		if (periode >= length - 5){
-			periode = 0;
-		}
-		else{
-			this.incubationPeriode = periode;
+			this.incubationPeriod = period;
 		}
 		if (length > 5){
 			this.diseaseLength = length;
@@ -32,23 +42,42 @@ public class Disease {
 			this.diseaseLength = 5;
 		}
 	}
-
+	/**
+	 * 
+	 * @return Returns the maximum spread distance
+	 */
 	public double getSpreadDistance() {
 		return spreadDistance;
 	}
-
+	
+	/**
+	 * 
+	 * @return Returns the chance of the disease spreading
+	 */
 	public int getSpreadChance() {
 		return spreadChance;
 	}
-
+	
+	/**
+	 * 
+	 * @return Returns damage
+	 */
 	public int getDamage() {
 		return damage;
 	}
-
-	public int getIncubationPeriode() {
-		return incubationPeriode;
+	
+	/**
+	 * 
+	 * @return Returns the incubation period
+	 */
+	public int getIncubationPeriod() {
+		return incubationPeriod;
 	}
-
+	
+	/**
+	 * 
+	 * @return Returns how long the disease lasts
+	 */
 	public int getDiseaseLength() {
 		return diseaseLength;
 	}
