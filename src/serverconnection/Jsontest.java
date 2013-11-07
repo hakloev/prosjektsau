185c9592da4478c102c91bc110f1412f79b6fd41
maxmelander <maxmelander@192.168.1.7>
2013-11-07 14:09:50 +0100

    method for getting arraylist with area objects in jsonhandler

--- src/serverconnection/Jsontest.java
+++ src/serverconnection/Jsontest.java
@@ -14,16 +14,7 @@ public class Jsontest {
 	public static void main(String[] args) {
 		NetHandler nethandler = new NetHandler();
 		nethandler.login("Simulering", "Simulering");
-		Farm farm = JsonHandler.parseJsonAndReturnFarm(nethandler.getFarmFromSheepID(1));
-		System.out.println("farmID: " + farm.getfarmID() + " Owner ID: " + farm.getOwnerID());
-		ArrayList<Area> areaList = farm.getAreaList();
-		for (Area area : areaList){
-			System.out.println("Area ID: " + area.getId());
-			for (Position pos : area.getAreaPoints()){
-				System.out.println("lat: " + pos.getLatitude());
-				System.out.println("long: " + pos.getLongitude());
-			}
-		}		
+		ArrayList<Area> areas = JsonHandler.parseJsonAndReturnAreas(nethandler.getAreas());		
 
 	}
 
