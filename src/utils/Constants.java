734641a5d1d9aa3076b01da309c029576dfc3b50
Håkon Ødegård Løvdal <hakloev@gmail.com>
2013-11-08 11:48:55 +0100

    Finished logpanel, only javadoc and gui change left, should be easy and fast

--- src/utils/Constants.java
+++ src/utils/Constants.java
@@ -3,6 +3,6 @@ package utils;
 public class Constants {
 
 	public static final String pathToHtml = "/utils/map.html";
-	public static final String title = "50 Shades of Sheep";
+	public static final String title = "Fifty Shades of Sheep";
 	
 }
